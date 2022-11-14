9137f94ace57950bb9226fc41c1c640c8e41b4e2
Oliver Forral <intrepion@gmail.com>
2022-11-13 18:05:11 -0700

    Added docker files.

---
+++ scripts/docker_build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker build --tag intrepion-saying-hello-json-rpc-server-rust-actix-web --file Dockerfile .
---
+++ scripts/docker_run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo docker run -p 8080:8080 intrepion-saying-hello-json-rpc-server-rust-actix-web
